aec46edcb6042baa8373db6f7f3f6693cc7722b6
Thainan Prado <thainan.cpv76@gmail.com>
2022-07-26 16:49:40 -0300

    Correção de middleware

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/currency')->middleware('auth:sanctum');
+Route::get('/notfound', function () {
+    return view('app');
+})->name('notfound');
+
 Route::get('{any}', function () {
     return view('app');
-})->where('any', '.*');
+})->where('any', '.*')->name('any');
+
